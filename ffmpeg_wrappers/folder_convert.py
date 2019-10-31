322ed75d7b3413682cd6f3011c52f6e4675d8144
Andrew Taylor <taylora5@g.cofc.edu>
2019-10-31 18:09:10 -0400

    fixed some bugs, added debugging-type prints

--- ffmpeg_wrappers/folder_convert.py
+++ ffmpeg_wrappers/folder_convert.py
@@ -5,7 +5,7 @@
 #output: converted files in "converted" folder or in specified output folder (if provided)
 
 import sys
-from os import listdir # ().: listdir;  ().path.: dirname,splitext,join,isfile,abspath
+from os import listdir,system # ().: listdir;  ().path.: dirname,splitext,join,isfile,abspath
 from os.path import dirname,splitext,join,isfile,abspath
 import glob
 from shutil import copy2
@@ -19,19 +19,19 @@ def folder_convert(folder_list,out_path=None):
 	extensions_to_keep=('mp3','wav','m4a','aiff') # keeping formats commonly supported by cd burners
 
 	for folder in folder_list:
+		
 		folder=abspath(folder)
+		print(folder)
 		files = [f for f in listdir(folder) if isfile(join(folder, f))] #hmm I like the more object oriented approach where each of the folder contents has an isFolder attribute; might want a new library
 		out_folder=out_path if out_path else folder # this was previously within file loop, hopefully I'm not missing something
-
+		
+		print(files)
 		# loop files
 		for base_fn,ext in files.rsplit('.',1) if '.' in files else (files,''):
 			if ext in extensions_to_convert:
 
-				command=f'ffmpeg -i %s %s'
-				% (join(folder,base_fn+'.'+ext),
-				   join(out_folder,base_fn+'.wav')
-
-				os.system(command)#relevant files converted to wav
+				command='ffmpeg -i %s %s' % join(folder,base_fn+'.'+ext),join(out_folder,base_fn+'.wav')
+				system(command) #relevant files converted to wav
 
 			elif ext in extensions_to_keep:
 				# copy the files to the output folder
@@ -44,6 +44,6 @@ if __name__ == "__main__":
 	import argparse
 	parser=argparse.ArgumentParser()
 	parser.add_argument('folders',nargs='+',help='One or more folders containing audio files to convert')
-	parser.add_argument('-o',nargs=1,metavar='out_path',default=None,help='Optional out path for "converted" folder')
+	parser.add_argument('-o',nargs=1,dest='out_path',metavar='out_path',default=None,help='Optional out path for "converted" folder')
 	args=parser.parse_args()
 	folder_convert(args.folders,args.out_path)
