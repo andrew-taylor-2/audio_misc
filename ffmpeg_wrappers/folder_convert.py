fdfb88b501e65e4926051adbbfe7ca4c9473e499
Andrew Taylor <taylora5@g.cofc.edu>
2019-11-01 20:59:29 -0400

    no longer unnecessarily implemented as class

--- ffmpeg_wrappers/folder_convert.py
+++ ffmpeg_wrappers/folder_convert.py
@@ -11,56 +11,49 @@ import glob
 from shutil import copy2
 import argparse
 
-class ActionRequest:
+def folder_convert(args):
+	
+	folders=args.folders
+	out_path=args.out_path if hasattr(args,'out_path') else None
+	# cases
+	extensions_to_convert=('flac','raw','ogg','mpc','webm')
+	extensions_to_keep=('mp3','wav','m4a','aiff') # keeping formats commonly supported by cd burners
 
-	#for debugging
-			#def __init__(self,input):
+	for folder in folders:
+		#folder=abspath(folder) # don't think this is actually necessary 
+		if out_path:
+			out_folder=out_path
+			mkdir(out_folder)
+		else: 
+			out_folder=folder
 
-	def __init__(self):
-		#parsers
-		parser=argparse.ArgumentParser()
-		subparsers=parser.add_subparsers()
-		cd_conv_parser=subparsers.add_parser('toCD')
-		cd_conv_parser.add_argument('folders',nargs='+',help='One or more folders containing audio files to convert')
-		cd_conv_parser.add_argument('-o',dest='out_path',metavar='out_path',default=None,help='Optional out path for "converted" folder')
-		cd_conv_parser.set_defaults(func=self.folder_convert)
+		for file in listdir(folder):
+			if isfile(join(folder,file)) and '.' in file:
+				print(file)
+				base_fn,ext=file.rsplit('.',1)
+				if ext in extensions_to_convert:
+					command='ffmpeg -i "%s" "%s"' % (join(folder,base_fn+'.'+ext),join(out_folder,base_fn+'.wav'))
+					system(command) #relevant files converted to wav
 
-		args=parser.parse_args()
-		#for debugging:
-			#args=parser.parse_args(input.split())
-			# self.args=args
-		args.func(args)
-		
+				elif ext in extensions_to_keep:
+					# copy the files to the output folder
+					copy2(join(folder,base_fn+'.'+ext),join(out_folder,base_fn+'.'+ext))
 
-	def folder_convert(self,args):
-		
-		folders=args.folders
-		out_path=args.out_path if hasattr(args,'out_path') else None
-		# cases
-		extensions_to_convert=('flac','raw','ogg','mpc','webm')
-		extensions_to_keep=('mp3','wav','m4a','aiff') # keeping formats commonly supported by cd burners
-
-		for folder in folders:
-			
-			folder=abspath(folder)
-
-			if out_path:
-				out_folder=out_path
-				mkdir(out_folder)
-			else: 
-				out_folder=folder
-
-			for file in listdir(folder):
-				if isfile(join(folder,file)) and '.' in file:
-					print(file)
-					base_fn,ext=file.rsplit('.',1)
-					if ext in extensions_to_convert:
-						command='ffmpeg -i "%s" "%s"' % (join(folder,base_fn+'.'+ext),join(out_folder,base_fn+'.wav'))
-						system(command) #relevant files converted to wav
-
-					elif ext in extensions_to_keep:
-						# copy the files to the output folder
-						copy2(join(folder,base_fn+'.'+ext),join(out_folder,base_fn+'.'+ext))
+# def compress_movie_audio():
 
 if __name__ == "__main__":
-	ActionRequest()
+	#parsers
+	parser=argparse.ArgumentParser()
+	subparsers=parser.add_subparsers()
+	cd_conv_parser=subparsers.add_parser('toCD')
+	cd_conv_parser.add_argument('folders',nargs='+',help='One or more folders containing audio files to convert')
+	cd_conv_parser.add_argument('-o',dest='out_path',metavar='out_path',default=None,help='Optional out path for "converted" folder')
+	cd_conv_parser.set_defaults(func=folder_convert)
+
+	#movie_comp_parser=subparsers.add_parser('compressMovie')
+
+	args=parser.parse_args()
+	#for debugging:
+		#args=parser.parse_args(input.split())
+		# self.args=args
+	args.func(args)
