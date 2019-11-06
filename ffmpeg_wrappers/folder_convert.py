af6dcc768cc54006cc52679806ec57883ba09212
Andrew Taylor <taylora5@g.cofc.edu>
2019-11-06 17:02:54 -0500

    dynamic rangee audio compression works now

--- ffmpeg_wrappers/folder_convert.py
+++ ffmpeg_wrappers/folder_convert.py
@@ -44,10 +44,30 @@ def folder_convert(args):
 def match_audio_to_power_histogram(args):
 	pass
 
+#def apply_audio_compression(mtype,movie,movie_out):
+#temporarily using the line below for debugging
 def apply_audio_compression(args):
 	#basically just call ffmpeg and "compand" based on which movie audio power contour type was selected
 	# if none has been selected, do the default/"secret recipe"
-	pass
+
+	#debugging
+	mtype=args.mtype
+	movie=args.movie
+	movie_out=args.movie_out
+
+	#horror_transfer
+	#comedy_transfer
+	#drama_transfer
+	default_transfer='compand=0 0:1 1:-90/-900 -70/-70 -30/-9 0/-3:6:0:0:0'
+
+	mt2c_in={None:default_transfer}
+	#mt2c_in={None:default_transfer,'horror':horror_transfer,'comedy':comedy_transfer,'drama':drama_transfer}
+	compand_inputs=mt2c_in[mtype]
+
+	command='ffmpeg -i "%s" -af "aformat=channel_layouts=stereo, compand=0 0:1 1:-90/-900 -70/-70 -30/-9 0/-3:6:0:0:0" "%s"' % (movie,movie_out) # this works for video or audio file, right?
+	system(command)
+
+
 
 
 if __name__ == "__main__":
@@ -61,11 +81,17 @@ if __name__ == "__main__":
 	cd_conv_parser.add_argument('-o',dest='out_path',metavar='out_path',default=None,help='Optional out path for "converted" folder')
 	cd_conv_parser.set_defaults(func=folder_convert)
 
-	compress_parser=subparsers.add_parser('toCD')
+	#subparser "compress" for match_audio_to_power_histogram
+	compress_parser=subparsers.add_parser('compress')
 	compress_parser.add_argument('movie',help='A movie file for which to compress audio')
-	compress_parser.set_defaults(func=match_audio_to_power_histogram)
+	compress_parser.add_argument('movie_out',help='Optional out path for "converted" folder')
+	compress_parser.add_argument('-type',dest='mtype',type=str,default=None,help='User specified type -- if given, skips matching algo')
 
-	#movie_comp_parser=subparsers.add_parser('compressMovie')
+	#compress_parser.set_defaults(func=match_audio_to_power_histogram)
+	#temporarily using the line below for debugging
+	compress_parser.set_defaults(func=apply_audio_compression)
+
+
 
 	args=parser.parse_args()
 	#for debugging:
